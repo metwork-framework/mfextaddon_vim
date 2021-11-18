8cc4864a3237ee6e516eb54a706e50728a4adf52
Jean-Baptiste VESLIN <jbaptiste31@free.fr>
2021-11-18 15:09:22 +0100

    feat: upgrade vim to patch 8.2.3614 to fix build on python 3.9 (#56)
    (cherry picked from commit 155901741ff5ed13298dc25241bd26c359584f7d)

--- layers/layer5_python3_vim/0050_vim/Makefile.mk
+++ layers/layer5_python3_vim/0050_vim/Makefile.mk
@@ -2,16 +2,17 @@ include ../../../adm/root.mk
 include $(MFEXT_HOME)/share/package.mk
 
 export NAME=vim
-export VERSION=8.2
-export EXTENSION=tar.bz2
+export VERSION=8.2.3614
+export EXTENSION=tar.gz
 export CHECKTYPE=MD5
-export CHECKSUM=2f3e6ab74d20eea2dc00cedc3f0ee4e8
+export CHECKSUM=49c93377bfb28a764337102136b5170f
 DESCRIPTION=\
-VIM est un editeur de texte unix
-WEBSITE=http://www.vim.org
+Vim is a highly configurable text editor for efficiently creating and changing any kind of text.
+WEBSITE=https://www.vim.org
 LICENSE=GPL
-SHORT_VERSION=82
+
+#Fix for python3.9 is included in patch 8.2.0354
 
 all:: $(PREFIX)/bin/vim
 $(PREFIX)/bin/vim:
-	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard EXPLICIT_NAME=vim$(SHORT_VERSION) PREFIX=$(PREFIX) OPTIONS="--enable-gui=no --with-local-dir=$(PREFIX) --with-python3-config-dir=$(PREFIX)/../python3_core/lib/python$(PYTHON3_SHORT_VERSION)/config-$(PYTHON3_SHORT_VERSION)-x86_64-linux-gnu --enable-python3interp=dynamic --enable-multibyte --with-features=huge --enable-cscope" download uncompress configure build install
+	$(MAKE) --file=$(MFEXT_HOME)/share/Makefile.standard PREFIX=$(PREFIX) OPTIONS="--enable-gui=no --with-local-dir=$(PREFIX) --with-python3-config-dir=$(PREFIX)/../python3_core/lib/python$(PYTHON3_SHORT_VERSION)/config-$(PYTHON3_SHORT_VERSION)-x86_64-linux-gnu --enable-python3interp=dynamic --enable-multibyte --with-features=huge --enable-cscope" download uncompress configure build install
